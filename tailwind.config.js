2619db60153f00473ed0f071a874222370cc3d6e
kanishkasd <kanishkasandaru2048@gmail.com>
2024-05-09 12:07:19 +0530

    apply custom styles and other useful styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        white: " hsl(0, 0%, 100%)",
-        "Rose White": "hsl(330, 100%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+        "Rose-White": "hsl(330, 100%, 98%)",
         Eggshell: "hsl(30, 54%, 90%)",
-        "Light Grey": "hsl(30, 18%, 87%)",
-        "Wenge Brown": "hsl(30, 10%, 34%)",
-        "Dark Charcoal": "hsl(24, 5%, 18%)",
+        "Light-Grey": "hsl(30, 18%, 87%)",
+        "Wenge-Brown": "hsl(30, 10%, 34%)",
+        "Dark-Charcoal": "hsl(24, 5%, 18%)",
 
         Nutmeg: "hsl(14, 45%, 36%)",
-        "Dark Raspberry": "hsl(332, 51%, 32%)",
+        "Dark-Raspberry": "hsl(332, 51%, 32%)",
       },
       fontFamily: {
         outfit: ["outfit", "sans"],
