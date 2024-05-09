6192e510fb9aab5f0e7f3b151f7f419cde887286
kanishkasd <kanishkasandaru2048@gmail.com>
2024-05-09 14:20:18 +0530

    did the mobile one

--- src/App.tsx
+++ src/App.tsx
@@ -44,61 +44,65 @@ const App = () => {
               </li>
             </ul>
 
-            <h3>Instruction</h3>
-            <ol>
+            <h3 className="font-young-serif text-2xl text-Nutmeg font-bold pt-5">
+              Instruction
+            </h3>
+            <ol className="custom-ordered-list font-outfit text-sm pt-5 text-Wenge-Brown">
               <li>
-                <span>Beat the eggs:</span> In a bowl, beat the eggs with a
-                pinch of salt and pepper until they are well mixed. You can add
-                a tablespoon of water or milk for a fluffier texture.
+                <span className="font-bold">Beat the eggs:</span> In a bowl,
+                beat the eggs with a pinch of salt and pepper until they are
+                well mixed. You can add a tablespoon of water or milk for a
+                fluffier texture.
               </li>
               <li>
-                <span>Heat the pan: </span> Place a non-stick frying pan over
-                medium heat and add butter or oil.
+                <span className="font-bold">Heat the pan: </span> Place a
+                non-stick frying pan over medium heat and add butter or oil.
               </li>
               <li>
-                <span>Cook the omelette:</span> Once the butter is melted and
-                bubbling, pour in the eggs. Tilt the pan to ensure the eggs
-                evenly coat the surface.
+                <span className="font-bold">Cook the omelette:</span> Once the
+                butter is melted and bubbling, pour in the eggs. Tilt the pan to
+                ensure the eggs evenly coat the surface.
               </li>
               <li>
-                <span>Add fillings(optional)</span>
+                <span className="font-bold">Add fillings(optional): </span>
                 When the eggs begin to set at the edges but are still slightly
                 runny in the middle, sprinkle your chosen fillings over one half
                 of the omelette.
               </li>
+              <li>
+                <span className="font-bold">Fold and serve:</span>
+                As the omelette continues to cook, carefully lift one edge and
+                fold it over the fillings. Let it cook for another minute, then
+                slide it onto a plate.
+              </li>
+              <li>
+                <span className="font-bold">Enjoy:</span>Serve hot, with
+                additional salt and pepper if needed.
+              </li>
             </ol>
-            <li>
-              <span>Fold and serve:</span>
-              As the omelette continues to cook, carefully lift one edge and
-              fold it over the fillings. Let it cook for another minute, then
-              slide it onto a plate.
-            </li>
-            <li>
-              <span>Enjoy:</span>Serve hot, with additional salt and pepper if
-              needed.
-            </li>
-
-            <h3>Nutrition</h3>
-            <p>
+            <h3 className="font-young-serif text-2xl text-Nutmeg font-bold pt-5">
+              Nutrition
+            </h3>
+            <p className="text-Wenge-Brown text-sm font-outfit pt-2 pb-5">
               The table below shows nutritional values per serving without the
               additional fillings.
             </p>
-            <table>
+            <table className="font-outfit text-Wenge-Brown ml-5">
               <tr className="border-b border-grey-200">
                 <td>Calories</td>
-                <td>277kcal</td>
+                <td className="px-10 py-2 text-Nutmeg font-bold">277kcal</td>
               </tr>
               <tr className="border-b border-grey-200">
                 <td>Carbs</td>
-                <td>0g</td>
+                <td className="px-10 py-2 text-Nutmeg font-bold">0g</td>
               </tr>
               <tr className="border-b border-grey-200">
                 <td>Protein</td>
-                <td>20g</td>
+                <td className="px-10 py-2 text-Nutmeg font-bold">20g</td>
               </tr>
               <tr className="border-b border-grey-200">
                 <td>Fat</td>
-                <td>22g</td>
+                <td className="px-10 py-2 text-Nutmeg font-bold">22g</td>
               </tr>
             </table>
           </div>
